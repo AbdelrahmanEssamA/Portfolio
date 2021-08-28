5af58d9e4b3fdfb8af8cbb854f95676ad43585d8
Abdelrahman <abdelrahman.essam@mindeavors.com>
2021-08-28 15:30:17 +0200

    added titles

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 function smoothScroll(target, duration) {
-   var target = document.querySelector(target);
-   var targetPosition = target.getBoundingClientRect().top;
-   var startPosition = window.pageYOffset;
-   var distance = targetPosition - startPosition;
-   var startTime = null;
-   function animation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      var timeElapsed = currentTime - startTime;
-      var run = ease(timeElapsed, startPosition, distance, duration);
-      window.scrollTo(0, run);
-      if (timeElapsed < duration) requestAnimationFrame(animation);
-   }
-   function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
-   }
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
 
-   requestAnimationFrame(animation);
+  requestAnimationFrame(animation);
 }
 //Animating to skills
 document.querySelector(".to-skills").addEventListener("click", function (e) {
-   e.preventDefault();
-   smoothScroll(".feature", 500);
+  e.preventDefault();
+  smoothScroll(".feature", 500);
 });
 
 //Animating to work
 document.querySelector(".to-work").addEventListener("click", function (e) {
-   e.preventDefault();
-   smoothScroll(".portfolio", 1000);
+  e.preventDefault();
+  smoothScroll(".portfolio-bg", 1000);
 });
 
 //Animating to contact
 document.querySelector(".to-contact").addEventListener("click", function (e) {
-   e.preventDefault();
-   smoothScroll(".contact-me", 2500);
+  e.preventDefault();
+  smoothScroll(".contact-me", 2500);
 });
 
 //Animating to top
 document.querySelector(".go-up").addEventListener("click", function (e) {
-   e.preventDefault();
-   smoothScroll(".logo", 2500);
+  e.preventDefault();
+  smoothScroll(".logo", 2500);
 });
 const scroll = document.querySelector(".scroll");
 var isDown = false;
@@ -50,30 +50,30 @@ var scrollLeft;
 
 // Mouse Up Function
 scroll.addEventListener("mouseup", () => {
-	isDown = false;
-	scroll.classList.remove("active");
+  isDown = false;
+  scroll.classList.remove("active");
 });
 
 // Mouse Leave Function
 scroll.addEventListener("mouseleave", () => {
-	isDown = false;
-	scroll.classList.remove("active");
+  isDown = false;
+  scroll.classList.remove("active");
 });
 
 // Mouse Down Function
 scroll.addEventListener("mousedown", (e) => {
-	e.preventDefault();
-	isDown = true;
-	scroll.classList.add("active");
-	scrollX = e.pageX - scroll.offsetLeft;
-	scrollLeft = scroll.scrollLeft;
+  e.preventDefault();
+  isDown = true;
+  scroll.classList.add("active");
+  scrollX = e.pageX - scroll.offsetLeft;
+  scrollLeft = scroll.scrollLeft;
 });
 
 // Mouse Move Function
 scroll.addEventListener("mousemove", (e) => {
-	if (!isDown) return;
-	e.preventDefault();
-	var element = e.pageX - scroll.offsetLeft;
-	var scrolling = (element - scrollX) * 2;
-	scroll.scrollLeft = scrollLeft - scrolling;
+  if (!isDown) return;
+  e.preventDefault();
+  var element = e.pageX - scroll.offsetLeft;
+  var scrolling = (element - scrollX) * 2;
+  scroll.scrollLeft = scrollLeft - scrolling;
 });
